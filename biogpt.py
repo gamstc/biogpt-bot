31cb8674df6d9ca7150604fdb5b970d93b21de84
gamstc <cornelia.gamst@gmail.com>
2024-03-06 13:10:12 +0100

    Create biogpt.py

---
+++ biogpt.py
@@ -0,0 +1,20 @@
+!pip install sacremoses
+!pip install transformers
+
+import streamlit as st
+from transformers import pipeline
+
+st.markdown("## Chatten mit BioGPT")
+
+st.markdown( "Hier können verschiedene Prompts mit BioGPT getestet werden." )
+
+st.markdown("---")
+
+
+prompt_list_dropdown = ["Prompt 1", 
+                        "Prompt 2", 
+                        "Prompt 3", 
+                        "Prompt 4"
+                       ]
+
+prompt_option = st.selectbox("Prompt Auswahl", prompt_list_dropdown)
