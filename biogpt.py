60861f9e467c98031189d8e7c6a26aa9ef372c27
gamstc <cornelia.gamst@gmail.com>
2024-03-13 16:06:40 +0100

    Chat interface

--- biogpt.py
+++ biogpt.py
@@ -2,6 +2,11 @@
 # !pip install transformers
 
 import streamlit as st
+
+#--------------------------------------------------------------------------------------------------
+# Hugging Face Models: Leaving them our for now. 
+
+
 #from transformers import pipeline, set_seed
 #from transformers import BioGptTokenizer, BioGptForCausalLM
 
@@ -39,6 +44,54 @@ st.markdown("---")
 #answer = generator(input_text, max_length=20, num_return_sequences=5, do_sample=True)
 
 #st.write(answer)
+
+
+
+#---------------------------------------------------------------------------------------------------------
+# Simple Chatbot interface: 
+
+import random
+import time
+
+
+# Streamed response emulator
+def response_generator():
+    response = random.choice(
+        [
+            "Hello there! How can I assist you today?",
+            "Hi, human! Is there anything I can help you with?",
+            "Do you need help?",
+        ]
+    )
+    for word in response.split():
+        yield word + " "
+        time.sleep(0.05)
+
+
+st.title("Simple chat")
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# Accept user input
+if prompt := st.chat_input("What is up?"):
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        response = st.write_stream(response_generator())
+    # Add assistant response to chat history
+    st.session_state.messages.append({"role": "assistant", "content": response})
 
 
 
